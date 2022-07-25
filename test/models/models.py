3735adbbe779227aaed92afeb725013a992dcc10
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-07-25 04:19:18 -0500

    test agregad

--- test/models/models.py
+++ test/models/models.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from tkinter import W
+from odoo import models, fields, api
 
 
-# class test(models.Model):
-#     _name = 'test.test'
-#     _description = 'test.test'
+class Curso(models.Model):
+    
+    _name = 'academia.curso'
+    _description = 'Descripción del cursos'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    title = models.CharField(required=True, help='Titulo del curso')
+    description = models.TextField(required=True, help='Edad del usuario')
+    responsable = models.Many2one('res.users', help='Responsable del curso', required=True)
+    start_date = models.DateField(help='Fecha de inicio del curso')
+    finish_date = models.DateField(help='Fecha de fin del curso')
+    
+    
+class Sesion(models.Model):
+    _name = 'academia.sesion'
+    _description = 'Descripción de las sesiones'
+    
+    curso = models.Many2one('academia.curso', help='Curso de la sesion')
+    instructor = models.Many2one('res.users', help='Instructor del curso')        
