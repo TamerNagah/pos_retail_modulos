95de39827a600859745ada7c2e9f335d9da39bb2
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-07-28 00:39:10 -0500

    asd

--- academy/models/student.py
+++ academy/models/student.py
@@ -11,22 +11,22 @@ class StudentStudent(models.Model):
     _name = 'student.student'
     _description = 'Informacion del estudiante'
     
-    # @api.model
-    # def _default_image(self):
-    #     '''Method to get default Image'''
-    #     image_path = get_module_resource('hr', 'static/src/img',
-    #                                      'default_alumno.jpg')
-    #     return base64.b64encode(open(image_path, 'rb').read())    
+    @api.model
+    def _default_image(self):
+        '''Method to get default Image'''
+        image_path = get_module_resource('hr', 'static/src/img',
+                                         'default_alumno.jpg')
+        return base64.b64encode(open(image_path, 'rb').read())    
     
-    # photo = fields.Binary('Foto', default=_default_image)
+    photo = fields.Binary('Foto', default=_default_image)
     pid = fields.Char('ID del estudiante',help='Numero Personal de Identificacion')
     name = fields.Char('Nombres', required=True)
     last_name = fields.Char('Apellido', required=True)
     # academy = fields.Many2one('academy.academy', 'Academia')
     academy = fields.Char('Academia')
     # year = fields.Many2one('academy.year', 'Año')
-    idioma = fields.Many2one('standard.idioma', 'Idioma')
-    clase = fields.Many2one('clase.clase', 'Clase')
+    # idioma = fields.Many2one('standard.idioma', 'Idioma')
+    # clase = fields.Many2one('clase.clase', 'Clase')
     
     phone = fields.Char('Telefono')
     email = fields.Char('Email')
